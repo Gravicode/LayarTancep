466e70592a6725805387586271c5ac2a22fc355c
mifmasterz <mifmasterz@yahoo.com>
2022-12-05 07:31:57 +0700

    init

---
+++ src/LayarTancep/Models/Entities.cs
@@ -0,0 +1,154 @@
+﻿using GemBox.Document;
+using Microsoft.CodeAnalysis;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.Design;
+using System.Reflection;
+
+namespace LayarTancep.Models
+{
+    #region helpers model
+    public class StorageObject
+    {
+        public string Name { get; set; }
+        public long Size { get; set; }
+        public string FileUrl { get; set; }
+        public string ContentType { get; set; }
+        public DateTime? LastUpdate { get; set; }
+        public DateTime? LastAccess { get; set; }
+    }
+    public class StorageSetting
+    {
+        public string EndpointUrl { get; set; } = "https://is3.cloudhost.id";
+        public string AccessKey { get; set; }
+        public string SecretKey { get; set; }
+        public string Region { get; set; } = "USWest1";
+        public string Bucket { get; set; }
+        public string BaseUrl { get; set; }
+        public bool Ssl { get; set; } = true;
+        public StorageSetting()
+        {
+
+        }
+        public StorageSetting(string Endpoint, string Accesskey, string Secretkey, string Region, string Bucket)
+        {
+            this.EndpointUrl = Endpoint;
+            this.AccessKey = Accesskey;
+            this.SecretKey = Secretkey;
+            this.Region = Region;
+            this.Bucket = Bucket;
+            GenerateBaseUrl();
+        }
+        public void GenerateBaseUrl()
+        {
+            this.BaseUrl = EndpointUrl + "/{bucket}/{key}";
+        }
+    }
+    public class OutputCls
+    {
+        public OutputCls()
+        {
+            this.IsSucceed = false;
+            this.Message = "";
+        }
+        public object Data { get; set; }
+        public string Message { get; set; }
+        public bool IsSucceed { get; set; }
+    }
+    #endregion
+
+
+
+    [Table("notification")]
+    public class Notification
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key, Column(Order = 0)]
+        public long Id { get; set; }
+
+        public DateTime CreatedDate { set; get; }
+        [ForeignKey(nameof(User)), Column(Order = 0)]
+        public long UserId { set; get; }
+        public UserProfile User { set; get; }
+        public string Action { set; get; }
+        public string LinkUrl { set; get; }
+        public string LinkDesc { set; get; }
+        public string Message { set; get; }
+        public bool IsRead { set; get; } = false;
+    }
+    public enum LogCategory
+    {
+        Info, Error, Warning
+    }
+    [Table("logs")]
+    public class Log
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key, Column(Order = 0)]
+        public long Id { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime LogDate { get; set; }
+        public string Message { get; set; }
+        public LogCategory Category { get; set; }
+    }
+
+    [Table("userprofile")]
+    public class UserProfile
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key, Column(Order = 0)]
+        public long Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string FullName { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+        public string? Alamat { get; set; }
+        public string? KTP { get; set; }
+        public string? PicUrl { get; set; }
+        public bool Aktif { get; set; } = true;
+        public string? Daerah { get; set; }
+        public string? Desa { get; set; }
+        public string? Kelompok { get; set; }
+        public Char Gender { get; set; } = 'N';
+        public Roles Role { set; get; } = Roles.User;
+        public DateTime CreatedDate { get; set; }
+        public double? Longitude { get; set; }
+        public double? Latitude { get; set; }
+
+        public string? FirstName { set; get; }
+        public string? LastName { set; get; }
+      
+        public string? AboutMe { set; get; }
+       
+        public string? FBUrl { set; get; }
+        public string? TwitterUrl { set; get; }
+        public string? GithubUrl { set; get; }
+        public string? InstagramUrl { set; get; }
+        public string? LinkedIdUrl { set; get; }
+
+
+    }
+
+    [Table("contact")]
+    public class Contact
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key, Column(Order = 0)]
+        public long Id { get; set; }
+
+        public string FullName { set; get; }
+        public string Email { set; get; }
+        public string Subject { set; get; }
+        public string Message { set; get; }
+        public DateTime CreatedDate { set; get; }
+        public string ReplyMessage { set; get; }
+        public string ReplyBy { set; get; }
+        public DateTime ReplyDate { set; get; }
+    }
+  
+
+    public enum Roles { Admin, User, Pengurus }
+
+
+}

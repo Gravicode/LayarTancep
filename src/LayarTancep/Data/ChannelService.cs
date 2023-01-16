655a266c3f56a6c089fd23de944ab45d9b1f2944
Janu <januartha.rn@gmail.com>
2023-01-16 14:32:54 +0700

    upload video

--- src/LayarTancep/Data/ChannelService.cs
+++ src/LayarTancep/Data/ChannelService.cs
@@ -37,6 +37,20 @@ namespace LayarTancep.Data
         {
             return db.Channels.OrderBy(x => x.Id).ToList();
         }
+        public List<Channel> GetByUser(UserProfile user)
+        {
+            
+            var datas =  db.Channels.Where(x=>x.UserName == user.Username).OrderBy(x => x.Name).ToList();
+            if (!datas.Any())
+            {
+                var newChannel = new Channel() { UserId = user.Id, UserName = user.Username, CreatedDate = DateHelper.GetLocalTimeNow(), Desc="Default Channel", Name = "My Videos", Category = "General"   };
+                db.Channels.Add(newChannel);
+                db.SaveChanges();
+                datas = db.Channels.Where(x => x.UserName == user.Username).OrderBy(x => x.Name).ToList();
+            }
+            
+            return datas;
+        }
 
         public Channel GetDataById(object Id)
         {
